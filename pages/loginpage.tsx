ad46d98592e39ee1a03b8c0239a6d765ce765b2b
Samarth <63744278+samarth777@users.noreply.github.com>
2023-03-19 18:04:15 +0530

    Update loginpage.tsx

--- pages/loginpage.tsx
+++ pages/loginpage.tsx
@@ -22,27 +22,6 @@ const LoginPage = () => {
   return (
     <div>
       <h2>Log In</h2>
-      <FormProvider {...methods}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <label htmlFor="">Email</label>
-            <input
-              type="email"
-              {...register("email", { required: "Email is required" })}
-            />
-            {errors.email && <p>{errors.email.message}</p>}
-          </div>
-          <div>
-            <label htmlFor="">Password</label>
-            <input
-              type="password"
-              {...register("password", { required: "Password is required" })}
-            />
-            {errors.password && <p>{errors.password.message}</p>}
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-      </FormProvider>
     </div>
   );
 };
